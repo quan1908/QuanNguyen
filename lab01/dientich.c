609ef88f2c45087bbd14beb5e150204d90b7aa0d
quan1908 <71130371+quan1908@users.noreply.github.com>
2020-09-11 18:42:09 +0700

    Create dientich.c

---
+++ lab01/dientich.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int r;
+	float P, S;
+	
+	printf("Nhap ban kinh r: ");
+	scanf("%d", &r);
+	
+	P = 2*r*3,14;
+	S = 3,14*r*r;
+	 printf("Chu vi hinh tron là: %f", P);
+	 printf("Dien tich hinh tron là: %f", S);
+	return 0;
+}
