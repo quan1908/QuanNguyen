af16be7615bac3a698d59cb50b3e3fa30fa5c9b5
quan1908 <71130371+quan1908@users.noreply.github.com>
2020-12-25 17:52:33 +0700

    Lab08

---
+++ Lab08/ASM2.sql
@@ -0,0 +1,99 @@
+﻿IF EXISTS (SELECT * FROM sys.databases WHERE NAME = 'Lab08')
+DROP DATABASE Lab08
+
+CREATE DATABASE Lab08
+
+USE Lab08
+GO
+
+CREATE TABLE SanPham(
+ProductID int,
+TenSP varchar(50),
+Description varchar(100) NULL,
+Donvi int,
+Price money CHECK(Price>0),
+SL int,
+CONSTRAINT PK_Product PRIMARY KEY (ProductID)
+)
+INSERT INTO SanPham VALUES
+(1,'Iphone','Like New','','1400',10),
+(2,'MacBook','New100%','','2800',5),
+(3,'Laptop','New100%','','2000',4),
+(4,'Galaxy','99%','','1400',10),
+(5,'Note10','Old','','1000',1)
+GO
+
+--Company là hãng
+CREATE TABLE Company(
+CompanyID int,
+CompanyName varchar(50),
+Phone varchar(20),
+Address varchar(100),
+CONSTRAINT PK_Company PRIMARY KEY (CompanyID)
+)
+INSERT INTO Company VALUES
+(1,'Asus','123456789','Taiwan'),
+(2,'Apple','123456780','USA'),
+(3,'Huawei','123456781','China'),
+(4,'Xiaomi','123456782','China'),
+(5,'SamSung','123456783','USA')
+GO
+CREATE TABLE Category(
+CategoryID int,
+CategoryName varchar(50),
+CONSTRAINT PK_Category PRIMARY KEY (CategoryID)
+)
+GO
+
+--4a:
+SELECT CompanyName FROM Company
+
+--4b:
+SELECT TenSP FROM SanPham
+
+--5a:
+SELECT CompanyName FROM Company
+ORDER BY CompanyName DESC
+
+--5b:
+SELECT TenSP,Price FROM SanPham
+ORDER BY Price DESC
+
+--5c:
+SELECT*FROM Company
+WHERE CompanyName='Asus'
+
+--5d:
+SELECT*FROM SanPham
+WHERE SL < 11 
+
+--6a:
+SELECT COUNT(CompanyName) AS N'Tổng sô hãng' FROM Company 
+
+--6b:
+SELECT COUNT(TenSP) AS N'Số mặt hàng' FROM SanPham
+
+--6c:
+SELECT TenSP,SL FROM SanPham
+
+--6D:
+SELECT SUM(SL) FROM SanPham
+
+--7A:
+ALTER TABLE SanPham
+ADD CONSTRAINT CHK_Price CHECK (Price>0)
+
+--7b:
+????
+
+--7c:
+ALTER TABLE SanPham
+ADD CONSTRAINT PK_Product PRIMARY KEY (ProductID)
+
+ALTER TABLE SanPham
+ADD CONSTRAINT PK_Company PRIMARY KEY (CompanyID)
+
+
+
+SELECT*FROM SanPham
+SELECT*FROM Company
---
+++ Lab08/ASM3.sql
@@ -0,0 +1,109 @@
+﻿IF EXISTS (SELECT * FROM sys.databases WHERE NAME = 'Lab08_1')
+DROP DATABASE Lab08_1
+
+CREATE DATABASE Lab08_1
+
+USE Lab08_1
+GO
+
+CREATE TABLE Customers(
+CustomersID int,
+TenKH varchar(50),
+CMND varchar(50) UNIQUE,
+Address varchar(100),
+CONSTRAINT PK_Customers PRIMARY KEY (CustomersID)
+)
+INSERT INTO Customers VALUES
+(1,'Nguyen Van A','123456789','Ha Noi'),
+(2,'Nguyen Van B','123456788','HCM'),
+(3,'Nguyen Van C','123456787','Hue'),
+(4,'Nguyen Van D','123456786','Thanh Hoa'),
+(5,'Nguyen Van E','123456785','Lang Son')
+GO
+
+CREATE TABLE Services(
+ServicesID int,
+ServicesName varchar(50),
+Description varchar(100) NULL,
+CONSTRAINT PK_Services PRIMARY KEY (ServicesID)
+)
+INSERT INTO Services VALUES
+(1,'Viettel','Tra Truoc'),
+(2,'Viettel','Tra Sau'),
+(3,'Vinaphone','Tra Truoc'),
+(4,'Mobifone','Tra Truoc'),
+(5,'Vinaphone','Tra Sau'),
+(6,'Mobifone','Tra Sau')
+GO
+
+CREATE TABLE CustomersDetails(
+CustomersID int,
+ServicesID int,
+Tel varchar(20),
+StartDate datetime,
+EndDate datetime,
+CONSTRAINT PK_ServicesCustomers PRIMARY KEY (ServicesID,CustomersID),
+CONSTRAINT FK_Customers FOREIGN KEY (CustomersID) REFERENCES Customers(CustomersID),
+CONSTRAINT FK_Services FOREIGN KEY (ServicesID) REFERENCES Services(ServicesID),
+)
+INSERT INTO CustomersDetails VALUES
+(1,1,'0123456789','2020-08-01','2022-08-01'),
+(2,5,'0123456789','2020-09-01','2020-09-01'),
+(3,4,'0123456789','2020-10-01','2020-10-01'),
+(4,2,'0123456789','2020-11-01','2020-11-01'),
+(5,3,'0123456789','2020-12-01','2020-12-01')
+GO
+
+
+
+--5a:
+SELECT * FROM  CustomersDetails JOIN Customers ON CustomersDetails.CustomersID = Customers.CustomersID
+WHERE Tel LIKE '0123456789'
+
+--5b:
+SELECT * FROM Customers JOIN CustomersDetails ON CustomersDetails.CustomersID = Customers.CustomersID
+WHERE CMND='123456789'
+
+--5c:
+SELECT Customers.TenKH,CustomersDetails.Tel FROM CustomersDetails JOIN Customers ON CustomersDetails.CustomersID = Customers.CustomersID
+WHERE CMND='123456789'
+
+--5d:
+SELECT CustomersDetails.Tel,CustomersDetails.StartDate FROM CustomersDetails
+WHERE StartDate = '2020-08-01'
+
+--5e:
+SELECT CustomersDetails.Tel FROM CustomersDetails JOIN Customers ON CustomersDetails.CustomersID=Customers.CustomersID
+WHERE Address='Ha Noi'
+
+--6a:
+SELECT COUNT(TenKH) AS N'Tổng số KH' FROM Customers
+
+--6b:
+SELECT COUNT(Tel) AS N'Tổng số thuê bao' FROM CustomersDetails
+
+--6c:
+SELECT StartDate,COUNT(Tel) FROM CustomersDetails
+WHERE StartDate='2020-08-01'
+GROUP BY StartDate
+
+--6D:
+SELECT*FROM Customers JOIN CustomersDetails ON CustomersDetails.CustomersID=Customers.CustomersID
+
+--7A:
+ALTER TABLE CustomersDetails 
+ALTER COLUMN StartDate Datetime NOT NULL
+
+--7b:
+ALTER TABLE CustomersDetails
+ADD CONSTRAINT CHK_DATE CHECK (DATEDIFF(DY,GETDATE(),StartDate)>=0)
+
+--7C:
+????
+
+--7D:
+????
+
+SELECT*FROM Customers
+SELECT*FROM Services
+SELECT*FROM CustomersDetails
