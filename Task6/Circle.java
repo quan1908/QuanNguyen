923e9900d8f3f948eea45f91404bd3fe28c544d5
quan1908 <71130371+quan1908@users.noreply.github.com>
2021-03-09 23:12:43 +0700

    Task6

---
+++ Task6/Circle.java
@@ -0,0 +1,27 @@
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle(){
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double r){
+        radius = r;
+        color = "red";
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+    public void setRadius(double newRadius){
+        radius = newRadius;
+    }
+    public String toString(){
+        return "Circle[radius=" + radius + "color=" + color + "]";
+    }
+}
---
+++ Task6/TestCircle.java
@@ -0,0 +1,19 @@
+public class TestCircle {
+    public static void main(String[] args){
+        Circle c1 = new Circle();
+        System.out.println("THe circle has radius of " + c1.getRadius() +
+                " and area of " + c1.getArea());
+
+        Circle c2 =  new Circle(2.0);
+        System.out.println("The circle has radius of " + c2.getRadius() +
+                " and area of " + c2.getArea());
+        Circle c4 =  new Circle();
+        c4.setRadius(5.0);
+        System.out.println("Radius is: " + c4.getRadius());
+
+        Circle c3 = new Circle(5.0);
+        System.out.println(c3.toString());
+        System.out.println(c3);
+        System.out.println("Operator '+' invokes toString() too: " + c3);
+    }
+}
