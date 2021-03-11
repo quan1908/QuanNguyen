686d34ba2a2e5c47a046cc9dd4923dc23a933346
quan1908 <71130371+quan1908@users.noreply.github.com>
2021-03-11 22:47:53 +0700

    Task8

---
+++ Task8/C.java
@@ -0,0 +1,10 @@
+public class C extends B {
+    public C() {  // Constructor
+        super();
+        System.out.println("Constructed an instance of C");
+    }
+    @Override
+    public String toString() {
+        return "This is C";
+    }
+}
